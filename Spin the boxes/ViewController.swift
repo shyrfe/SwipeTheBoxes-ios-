a10e02c3b2cf67845db569648647a9926a611904
Vladislav <vshurygin@mac-vladislav.local>
2016-10-07 02:45:41 -0700

    Initial Commit

---
+++ Spin the boxes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Spin the boxes
+//
+//  Created by Vladislav on 07.10.16.
+//  Copyright © 2016 Vladislav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
